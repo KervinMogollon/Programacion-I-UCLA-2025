d1a55861d8a3bc92f76aadab9049ceb813321520
KervinM <kervinM1812@gmail.com>
2025-02-19 19:47:57 -0400

    a

--- Otros/Prof/L25.1.p.c1.06.Aerolinea-main/js/Cl_controlador.js
+++ Otros/Prof/L25.1.p.c1.06.Aerolinea-main/js/Cl_controlador.js
@@ -1,10 +1,9 @@
 export default class Cl_controlador {
-  
   constructor(modelo, vista) {
     this.modelo = modelo;
     this.vista = vista;
   }
-  
+
   agregarVuelo() {
     this.modelo.procesarVuelo(this.vista.agregarVuelo());
     this.vista.reportarVuelo(
--- Practicas Casa/Corte 1 MVC/Ganancia articulos/js/CL_mTienda.js
+++ Practicas Casa/Corte 1 MVC/Ganancia articulos/js/CL_mTienda.js
@@ -13,6 +13,4 @@ export default class CL_mTienda{
             this.codMayor = art.cod
         }
     }
-
-
 }
